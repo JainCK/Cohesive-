46784d821b7d443dda49bbcf542e1d74e6f51e5c
zippyzag88 <kuriakosejain456@gmail.com>
2023-07-04 19:03:46 +0530

    added styling and js script for popup feature for get quuote button

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,23 @@
+// Get the popup element
+const popup = document.getElementById('quote-popup');
+
+// Get the button to open the popup
+const getQuoteButton = document.querySelector('.get-quote-button');
+
+// Get the button to close the popup
+const closePopupButton = document.querySelector('.close-popup-button');
+
+// Function to open the popup
+function openPopup() {
+  popup.style.display = 'block';
+}
+
+// Function to close the popup
+function closePopup() {
+  popup.style.display = 'none';
+}
+
+// Add event listeners
+getQuoteButton.addEventListener('click', openPopup);
+closePopupButton.addEventListener('click', closePopup);
  
