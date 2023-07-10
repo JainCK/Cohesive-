f83abf6eae428c01cd1b22f8206a669f4f8ea134
zippyzag88 <kuriakosejain456@gmail.com>
2023-07-10 23:34:10 +0530

    added responsiviness and cancel button for popup

--- scripts/index.js
+++ scripts/index.js
@@ -7,6 +7,15 @@ const getQuoteButton = document.querySelector('.get-quote-button');
 // Get the button to close the popup
 const closePopupButton = document.querySelector('.close-popup-button');
 
+// Get the cancel button
+const cancelButton = document.querySelector('.cancel-button');
+
+// Get the hamburger menu icon element
+const hamburgerMenu = document.querySelector('.hamburger-menu');
+
+// Get the navigation menu element
+const navMenu = document.querySelector('nav ul');
+
 // Function to open the popup
 function openPopup() {
   popup.style.display = 'block';
@@ -17,7 +26,25 @@ function closePopup() {
   popup.style.display = 'none';
 }
 
+// Function to handle cancel button click
+function handleCancel(event) {
+  event.preventDefault();
+  closePopup();
+
+  window.location.href = 'index.html';
+}
+
+
+// Function to toggle the navigation menu
+function toggleMenu() {
+  navMenu.classList.toggle('active');
+}
+
+
 // Add event listeners
 getQuoteButton.addEventListener('click', openPopup);
 closePopupButton.addEventListener('click', closePopup);
- 
+cancelButton.addEventListener('click', handleCancel);
+hamburgerMenu.addEventListener('click', toggleMenu);
+
+
